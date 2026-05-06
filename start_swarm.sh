7c5018f28d6df8419f96f347badfa9202cf43642
BulletproofBoris <santakloun1702@gmail.com>
2026-05-06 21:28:56 +0300

    .

--- start_swarm.sh
+++ start_swarm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. Задаем команду (я обернул ее в переменную для красоты и удобства)
-CMD='python run_walkforward.py --dataset_dir "data/processed/2000_2026_1d_18_3" --bonus_ratio 0.15 --min_delta 0.001 --runs 50 --batch_size 2048 --epochs 100 --l2_reg "1e-3" --lr "8e-2" --start_fold "fold_2016" --append'
+CMD='python run_walkforward.py --dataset_dir "data/processed/2000_2026_1d_90_15" --bonus_ratio 0.15 --min_delta 0.001 --runs 30 --batch_size 2048 --epochs 100 --l2_reg "1e-3" --lr "8e-2" --start_fold "fold_2010" --append'
 
 # 2. Список твоих tmux-сессий
 SESSIONS=(
