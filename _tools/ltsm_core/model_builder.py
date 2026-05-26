d0362c80aa880c329b9056728d5415d433605426
BulletproofBoris <santakloun1702@gmail.com>
2026-05-26 17:53:30 +0300

    .

--- _tools/ltsm_core/model_builder.py
+++ _tools/ltsm_core/model_builder.py
@@ -26,12 +26,12 @@ def create_model(seq_len, n_features, l2_reg):
     # ==========================================
     # ⚡ ВЕТКА 1: АДАПТИВНЫЙ ИМПУЛЬС (CNN)
     # ==========================================
-    p = 1
+    p = 0.05 * seq_len
     f = 12
-    kernel_1 = max(1, math.ceil(seq_len * p * 1))
-    kernel_2 = max(1, math.ceil(seq_len * p * 3))
-    kernel_3 = max(1, math.ceil(seq_len * p * 5))
-    kernel_4 = max(1, math.ceil(seq_len * p * 7))
+    kernel_1 = max(1, math.ceil(p * 1))
+    kernel_2 = max(2, math.ceil(p * 3))
+    kernel_3 = max(3, math.ceil(p * 5))
+    kernel_4 = max(4, math.ceil(p * 7))
     
     # Убрали лишние LayerNorm, добавили Dropout для регуляризации фильтров
     conv_1 = Conv1D(filters=f, kernel_size=kernel_1, padding='causal', activation='relu')(x)
